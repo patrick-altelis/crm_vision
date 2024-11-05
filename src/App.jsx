60b72ce6ccef85eb39d26c3984d98b3313218559
Patrick Guillaume <patrick_guillaume@mac-studio-de-patrick-1.home>
2024-11-05 15:31:16 +0100

    Installation react-router-dom et lucide-react, mise à jour du titre

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import EditableClientGrid from './components/EditableClientGrid'
 function App() {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Gestion des Clients</h1>
+      <h1 className="text-2xl font-bold mb-4">CRM BDD - Gestion des Clients</h1>
       <EditableClientGrid />
     </div>
   )
